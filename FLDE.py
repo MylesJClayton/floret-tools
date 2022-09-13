3be275e359b6db30cbdfde0d10175fd724eeb1eb
MylesJClayton <myc3@aber.ac.uk>
2022-09-13 17:42:08 +0100

    removed unused methods from FLDE.y

--- FLDE.py
+++ FLDE.py
@@ -22,6 +22,7 @@ from radiomics import shape
 import six
 import csv
 import numpy as np
+import argparse 
 
 #Choose a suitable filepath and file names (filenames must end in seg.nii.gz)
 os.chdir("/data/DATA/NPPC/FTcavitytest/Untitled Folder")
@@ -38,9 +39,8 @@ print(str(len(FloretFileNamesList)) + " floret labels were found")
 
 
 fieldnames = [ "FloretName", "Elongation", "Flatness", "LeastAxisLength", "MajorAxisLength", "Maximum2DDiameterColumn", "Maximum2DDiameterRow", "Maximum2DDiameterSlice", "Maximum3DDiameter", "MeshVolume", "MinorAxisLength", "Sphericity", "SurfaceArea", "SurfaceVolumeRatio", "VoxelVolume"]
-GrainFieldnames = [ "FloretName", "Elongation", "Flatness", "LeastAxisLength", "MajorAxisLength", "Maximum2DDiameterColumn", "Maximum2DDiameterRow", "Maximum2DDiameterSlice", "Maximum3DDiameter", "MeshVolume", "MinorAxisLength", "Sphericity", "SurfaceArea", "SurfaceVolumeRatio", "VoxelVolume", "PercentageOfCavityFilled", "BranPercentage"]
 with open('GrainData.csv', mode='w', newline='') as GrainData_csv:
-    writer = csv.DictWriter(GrainData_csv, fieldnames=GrainFieldnames)
+    writer = csv.DictWriter(GrainData_csv, fieldnames=fieldnames)
     writer.writeheader()
 with open('CavityData.csv', mode='w', newline='') as CavityData_csv:
     writer = csv.DictWriter(CavityData_csv, fieldnames=fieldnames)
@@ -59,6 +59,9 @@ reader = sitk.ImageFileReader()
 reader.SetImageIO("NiftiImageIO")
 sitkwriter = sitk.ImageFileWriter()
 getstats = sitk.StatisticsImageFilter()
+DilateImage = sitk.BinaryDilateImageFilter()
+ErodeImage = sitk.BinaryErodeImageFilter()
+    
 CompleteCounter = 0
 #For each listed floret label, create a cavity label and extract statistics for each part
 for FloretFilename in FloretFileNamesList:
@@ -69,29 +72,21 @@ for FloretFilename in FloretFileNamesList:
     FloretLabel.SetSpacing((0.034, 0.034, 0.034)) #resolution of CT scanner in milimeters
     
     #obtain shell mask by thresholding labels 4 and 5 which are the palea and lemma respectively 
+    GrainMask = sitk.BinaryThreshold( FloretLabel, 1, 1, 1, 0 )
     PaleaMask = sitk.BinaryThreshold( FloretLabel, 2, 2, 1, 0 )
     LemmaMask = sitk.BinaryThreshold( FloretLabel, 3, 3, 1, 0 )
-    CapMask = sitk.BinaryThreshold( FloretLabel, 4, 4, 1, 0 )
-    ShellMask = PaleaMask + LemmaMask + CapMask #Shell mask obtained by elementwise addition of the masks
+    ShellMask = PaleaMask + LemmaMask #Shell mask obtained by elementwise addition of the masks
     ShellMaskNoCap = PaleaMask + LemmaMask
     
     #save result
     sitkwriter.SetFileName(str(FloretFilename) + "ShellMask.nii.gz")
     sitkwriter.Execute(ShellMask)
-    #Create masks of grain, bran layer and embryo
-    GrainMask = sitk.BinaryThreshold( FloretLabel, 1, 1, 1, 0 )
-    BranMask = sitk.BinaryThreshold( FloretLabel, 7, 7, 1, 0 )
-    
-    #Merge masks
-    TotalGrainMask = GrainMask + BranMask
-    
+        
     #perform dilation on shell mask to form enclosed region
     #Dilation number (number of iterations of dilaion) is manually chosen
     #based on the size of the gap between the lemma and palea which needs to be filled
-    ShellMaskDilated = ShellMask
-    DilateImage = sitk.BinaryDilateImageFilter()
-    ErodeImage = sitk.BinaryErodeImageFilter()
-    
+
+    """ Use this when doing grain growth
     dilationnumber = 12
     dilationcounter1 = 0
     print("dilating shell")
@@ -99,74 +94,7 @@ for FloretFilename in FloretFileNamesList:
     while dilationcounter1 < dilationnumber:
         ShellMaskDilated = DilateImage.Execute(ShellMaskDilated)
         dilationcounter1 = dilationcounter1 + 1
-    
-    print("done")
-    #Fill dilated shell to get dilated shell + cavity 
-    #cavity has been shrunk due to shell dilation
-    FillCavity = sitk.BinaryFillholeImageFilter()
-    ShellandCavity = FillCavity.Execute(ShellMaskDilated)
-    
-    #subtract dilated shell from the sum , leaving shrunk cavity
-    CavityShrunk = ShellandCavity - ShellMaskDilated
-    
-    #Dilate cavity equal number of times to dilation counter
-    CavityDilation = CavityShrunk
-    dilationcounter2 = 0
-    print("dilating cavity")
-    while dilationcounter2 < dilationcounter1:
-        CavityDilation = DilateImage.Execute(CavityDilation)
-        dilationcounter2 = dilationcounter2 + 1
-        #Creates a smoothed cavity with quite a bit of unlabelled empty space near inside corners
-    
-    ShellMask3Dilations = DilateImage.Execute(DilateImage.Execute(DilateImage.Execute(ShellMask)))
-    DilateErodeLeakIterations = 0
-    while DilateErodeLeakIterations < 3:
-        print("done")
-        #Expand to fill area, subtract shellmask from cavity, remove parts that overstepped the shell
-        FillIterations = 5
-        Iterationcounter = 0 
-        print("performing 4 iterations of dilation and shell subtraction")
-        while Iterationcounter < FillIterations:
-            CavityDilation = DilateImage.Execute(CavityDilation)
-            CavityDilation = CavityDilation - ShellMask3Dilations + 1
-            CavityDilation = sitk.BinaryThreshold( CavityDilation, 2, 2, 1, 0 ) #possible outcomes: 0-0+1=1 1-0+1=2 0-1+1=0 1-1+1=1
-            Iterationcounter = Iterationcounter + 1
-            #This causes a small amount of the label to leak through the gaps in the shell 
-            #They are thin and can be eroded away
-            
-        print("done")
-         
-        #Erode 3 times and dilate 4 times (+1 to regain accuracy) and subtract shell again
-        ErodeIterations = 3
-        ErodeCounter = 0
-        dilationcounter3 = 0
-        print("eroding to remove leak")
-        while ErodeCounter < ErodeIterations:
-            CavityDilation = ErodeImage.Execute(CavityDilation)
-            ErodeCounter = ErodeCounter + 1
-            
-            print("dilating to refill cavity")
-            while dilationcounter3 < ErodeIterations + 3:
-                CavityDilation = DilateImage.Execute(CavityDilation)
-                dilationcounter3 = dilationcounter3 + 1
-                
-        DilateErodeLeakIterations = DilateErodeLeakIterations +1
-          
-    
-    CavityDilation = DilateImage.Execute(CavityDilation)
-    CavityDilation = CavityDilation - ShellMaskNoCap + 1
-    CavityDilation = sitk.BinaryThreshold( CavityDilation, 2, 2, 1, 0 )
-    CavityDilation = DilateImage.Execute(CavityDilation)
-    CavityDilation = CavityDilation - ShellMaskNoCap + 1
-    CavityDilation = sitk.BinaryThreshold( CavityDilation, 2, 2, 1, 0 )
-    CavityLabel = CavityDilation
-    
-    
-    print("done")
-   
-    #save result cavity label
-    sitkwriter.SetFileName(str(FloretFilename) + "CavityLabel.nii.gz")
-    sitkwriter.Execute(CavityLabel)
+    """
     
     "This section is for using pyradiomics to extract sphericity; length, width and height values; "
     #Define Dictionaries in which the first the first entry is the name of the floret
@@ -209,7 +137,7 @@ for FloretFilename in FloretFileNamesList:
 
     # Calculate the features, print result and append to the dictionary
     print('Calculating shape features for ' + str(FloretFilename) + ' Grain...',)
-    shapeFeatures = shape.RadiomicsShape(FloretLabel, TotalGrainMask)
+    shapeFeatures = shape.RadiomicsShape(FloretLabel, GrainMask)
     
     result = shapeFeatures.execute()
     print('done')
@@ -219,24 +147,14 @@ for FloretFilename in FloretFileNamesList:
         GrainFeatures[key] = np.round(val, 4)
         print('  ', key, ':', np.round(val, 4))
     
-    Branstats = getstats.Execute(BranMask)
-    BranVoxelSum = getstats.GetSum()
-    BranVolume = BranVoxelSum * 0.034**3
-    GrainFeatures
-    print(GrainFeatures)
-    GrainFeatures["PercentageOfCavityFilled"] = GrainFeatures["VoxelVolume"] / CavityFeatures["VoxelVolume"]
-    GrainFeatures["BranPercentage"] =  BranVolume / GrainFeatures["VoxelVolume"]
-
     with open('GrainData.csv', mode='a', newline='') as GrainData_csv:
-        writer = csv.DictWriter(GrainData_csv, fieldnames=GrainFieldnames)
+        writer = csv.DictWriter(GrainData_csv, fieldnames=fieldnames)
         writer.writerow(GrainFeatures)
-
     
     
     # Calculate the features, print result and append to the dictionary
     print('Calculating shape features for ' + str(FloretFilename) + ' Palea...',)
     shapeFeatures = shape.RadiomicsShape(FloretLabel, PaleaMask)
-        
     result = shapeFeatures.execute()
     print('done')
     
@@ -248,8 +166,6 @@ for FloretFilename in FloretFileNamesList:
     with open('PaleaData.csv', mode='a', newline='') as PaleaData_csv:
         writer = csv.DictWriter(PaleaData_csv, fieldnames=fieldnames)
         writer.writerow(PaleaFeatures)
-    
-    
     
 
     # Calculate the features, print result and append to the dictionary
@@ -266,6 +182,7 @@ for FloretFilename in FloretFileNamesList:
     with open('LemmaData.csv', mode='a', newline='') as LemmaData_csv:
         writer = csv.DictWriter(LemmaData_csv, fieldnames=fieldnames)
         writer.writerow(LemmaFeatures)
+
 
     # Calculate the features, print result and append to the dictionary
     print('Calculating shape features ' + str(FloretFilename) + ' for Shell...',)
@@ -288,11 +205,28 @@ for FloretFilename in FloretFileNamesList:
     
 
 print("Data extraction completed for all listed floret labels")
-print("Have a good day :)")
     
     
+if __name__ == '__main__':
     
+    parser = argparse.ArgumentParser(description=(
+        "Analyse the shape of labels in the *seg.nii.gz files in the specified folder."
+        "Save extracted shape statistics in .csv files."
+    ))
     
-    
+    parser.add_argument('-i', '--inpath',
+                        type=str, default=None,
+                        help='Input directory absolute path')
+    parser.add_argument('-o', '--outpath',
+                        type=str, default=None,
+                        help='Output directory absolute path')
+    parser.add_argument('-t', '--troubleshoot', action='store_true',
+                        help='Save images  of intermediate steps threshold, components, and mask')
+    parser.add_argument('-g', '--troubleshoot', action='store_true',
+                        help='Save images  of intermediate steps threshold, components, and mask')
+
+    args = parser.parse_args()
+    print(args, flush=True)
+    main(args.inpath,args.outpath,troubleshoot=args.troubleshoot,normalize=args.normalize)
     
     
