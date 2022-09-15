ddb83d9fb463035b0c796fe0fae19e05e8bc75c2
MylesJClayton <myc3@aber.ac.uk>
2022-09-15 17:24:08 +0100

    Rewritten with functions and argparse

--- FLDE.py
+++ FLDE.py
@@ -24,187 +24,212 @@ import csv
 import numpy as np
 import argparse 
 
-#Choose a suitable filepath and file names (filenames must end in seg.nii.gz)
-os.chdir("/data/DATA/NPPC/FTcavitytest/Untitled Folder")
-FloretFileNamesList = []
+def MkDirDict(InDirectory,OutDirectory):
+    "builds dictionary of filepaths to use later"
+    
+    
+    DirDict = {
+        'in': InDirectory,
+        'out': OutDirectory,
+        'shellmask': OutDirectory + "/ShellMasks"
+        }
+    for path in DirDict:
+        if not os.path.exists(DirDict[path]):
+            os.makedirs(DirDict[path])
+    
+    return(DirDict)
 
-#Search for all files in directory that end with seg.nii.gz
-for file in os.listdir("/data/DATA/NPPC/FTcavitytest/Untitled Folder"):
-    if file.endswith("seg.nii.gz"):
-        file = file[:-10]
-        FloretFileNamesList.append(file)
-        print(file)
-        
-print(str(len(FloretFileNamesList)) + " floret labels were found")
 
+def MkInputList(directory):
+    "finds all files in folder ending with nii.gz and adds them to a list"
+    
+    ImageFileList = []
+    
+    for file in os.listdir(directory):
+        if file.endswith("seg.nii.gz"):
+            file = file[:-10]
+            ImageFileList.append(file)
+    
+    print(str(len(ImageFileList)) + " Input images were found" , flush=True)
+    return(ImageFileList)
+     
+def GrainGrowingSpace(GrainMask,ShellMask,grownumber):
+    DilateImage = sitk.BinaryDilateImageFilter()
+    ErodeImage = sitk.BinaryErodeImageFilter()
+    getstats = sitk.StatisticsImageFilter()
+    if grownumber == 0:
+        EmptyLabel = GrainMask - GrainMask
+        return(EmptyLabel) #This is an image the same size as the image but contains only zeroes.  
+    else:
+        dilationnumber = grownumber
+        dilationcounter = 0
+        print("dilating grain")
 
-fieldnames = [ "FloretName", "Elongation", "Flatness", "LeastAxisLength", "MajorAxisLength", "Maximum2DDiameterColumn", "Maximum2DDiameterRow", "Maximum2DDiameterSlice", "Maximum3DDiameter", "MeshVolume", "MinorAxisLength", "Sphericity", "SurfaceArea", "SurfaceVolumeRatio", "VoxelVolume"]
-with open('GrainData.csv', mode='w', newline='') as GrainData_csv:
-    writer = csv.DictWriter(GrainData_csv, fieldnames=fieldnames)
-    writer.writeheader()
-with open('CavityData.csv', mode='w', newline='') as CavityData_csv:
-    writer = csv.DictWriter(CavityData_csv, fieldnames=fieldnames)
-    writer.writeheader()
-with open('PaleaData.csv', mode='w', newline='') as PaleaData_csv:
-    writer = csv.DictWriter(PaleaData_csv, fieldnames=fieldnames)
-    writer.writeheader()
-with open('LemmaData.csv', mode='w', newline='') as LemmaData_csv:
-    writer = csv.DictWriter(LemmaData_csv, fieldnames=fieldnames)
-    writer.writeheader()
-with open('ShellData.csv', mode='w', newline='') as ShellData_csv:
-    writer = csv.DictWriter(ShellData_csv, fieldnames=fieldnames)
-    writer.writeheader()
-   
-reader = sitk.ImageFileReader()
-reader.SetImageIO("NiftiImageIO")
-sitkwriter = sitk.ImageFileWriter()
-getstats = sitk.StatisticsImageFilter()
-DilateImage = sitk.BinaryDilateImageFilter()
-ErodeImage = sitk.BinaryErodeImageFilter()
-    
-CompleteCounter = 0
-#For each listed floret label, create a cavity label and extract statistics for each part
-for FloretFilename in FloretFileNamesList:
-    print("Finding Cavity for " + str(FloretFilename))
-    #read segmentation file
-    reader.SetFileName(str(FloretFilename) + "seg.nii.gz")
-    FloretLabel = reader.Execute();
-    FloretLabel.SetSpacing((0.034, 0.034, 0.034)) #resolution of CT scanner in milimeters
-    
-    #obtain shell mask by thresholding labels 4 and 5 which are the palea and lemma respectively 
-    GrainMask = sitk.BinaryThreshold( FloretLabel, 1, 1, 1, 0 )
-    PaleaMask = sitk.BinaryThreshold( FloretLabel, 2, 2, 1, 0 )
-    LemmaMask = sitk.BinaryThreshold( FloretLabel, 3, 3, 1, 0 )
-    ShellMask = PaleaMask + LemmaMask #Shell mask obtained by elementwise addition of the masks
-    ShellMaskNoCap = PaleaMask + LemmaMask
-    
-    #save result
-    sitkwriter.SetFileName(str(FloretFilename) + "ShellMask.nii.gz")
-    sitkwriter.Execute(ShellMask)
-        
-    #perform dilation on shell mask to form enclosed region
-    #Dilation number (number of iterations of dilaion) is manually chosen
-    #based on the size of the gap between the lemma and palea which needs to be filled
+        Space = GrainMask
+        while dilationcounter < dilationnumber:
+            Space = DilateImage.Execute(Space)
+            Space = Space - ShellMask + 1
+            CavityDilation = sitk.BinaryThreshold( CavityDilation, 2, 2, 1, 0 )
+            dilationcounter =+ 1
+        Space = Space - GrainMask
 
-    """ Use this when doing grain growth
-    dilationnumber = 12
-    dilationcounter1 = 0
-    print("dilating shell")
-    
-    while dilationcounter1 < dilationnumber:
-        ShellMaskDilated = DilateImage.Execute(ShellMaskDilated)
-        dilationcounter1 = dilationcounter1 + 1
-    """
-    
-    "This section is for using pyradiomics to extract sphericity; length, width and height values; "
-    #Define Dictionaries in which the first the first entry is the name of the floret
-    GrainFeatures = {
-            "FloretName" : FloretFilename
-            }
-    CavityFeatures = {
-            "FloretName" : FloretFilename
-            }
-    PaleaFeatures = {
-            "FloretName" : FloretFilename
-            }
-    LemmaFeatures = {
-            "FloretName" : FloretFilename
-            }
-    ShellFeatures = {
-            "FloretName" : FloretFilename
-            }
-    
-    print('Calculating shape features for ' + str(FloretFilename) + ' Cavity...',)
-    shapeFeatures = shape.RadiomicsShape(FloretLabel, CavityLabel)
-    
-    # Set the features to be calculated
-    # shapeFeatures.enableFeatureByName('Volume', True)
-    shapeFeatures.enableAllFeatures()
-
-    # Calculate the features, print result and append to the dictionary
-    result = shapeFeatures.execute()
-    print('done')
-    
-    print('Calculated shape features for ' + str(FloretFilename) + ' Cavity: ')
-    for (key, val) in six.iteritems(result):
-        CavityFeatures[key] = np.round(val, 4)
-        print('  ', key, ':', np.round(val, 4))
-    
-    with open('CavityData.csv', mode='a', newline='') as CavityData_csv:
-        writer = csv.DictWriter(CavityData_csv, fieldnames=fieldnames)
-        writer.writerow(CavityFeatures)
+        getstats.Execute(Space)
+        volume = getstats.GetSum() * 0.034**3 #voxel count multiplied by milimeters per voxel
+        return(Space,volume)
     
 
-    # Calculate the features, print result and append to the dictionary
-    print('Calculating shape features for ' + str(FloretFilename) + ' Grain...',)
-    shapeFeatures = shape.RadiomicsShape(FloretLabel, GrainMask)
-    
-    result = shapeFeatures.execute()
-    print('done')
-    
-    print('Calculated shape features for ' + str(FloretFilename) + ' Grain: ')
-    for (key, val) in six.iteritems(result):
-        GrainFeatures[key] = np.round(val, 4)
-        print('  ', key, ':', np.round(val, 4))
-    
-    with open('GrainData.csv', mode='a', newline='') as GrainData_csv:
-        writer = csv.DictWriter(GrainData_csv, fieldnames=fieldnames)
-        writer.writerow(GrainFeatures)
-    
-    
-    # Calculate the features, print result and append to the dictionary
-    print('Calculating shape features for ' + str(FloretFilename) + ' Palea...',)
-    shapeFeatures = shape.RadiomicsShape(FloretLabel, PaleaMask)
-    result = shapeFeatures.execute()
-    print('done')
-    
-    print('Calculated shape features for ' + str(FloretFilename) + ' Palea: ')
-    for (key, val) in six.iteritems(result):
-            PaleaFeatures[key] = np.round(val, 4)
-            print('  ', key, ':', np.round(val, 4))
-    
-    with open('PaleaData.csv', mode='a', newline='') as PaleaData_csv:
+def main(inpath,outpath,growingspace=0,troubleshoot=False):
+    """Extract measurements from each label """
+
+    #Initialize functions and filters
+    reader = sitk.ImageFileReader()
+    reader.SetImageIO("NiftiImageIO")
+    sitkwriter = sitk.ImageFileWriter()
+    getstats = sitk.StatisticsImageFilter()
+
+    DirDict = MkDirDict(inpath,outpath)
+    InputLabelList = MkInputList(inpath)
+
+    print(str(len(InputLabelList)) + " floret labels were found")
+
+    fieldnames = [ "FloretName", "Elongation", "Flatness", "LeastAxisLength", "MajorAxisLength", "Maximum2DDiameterColumn", "Maximum2DDiameterRow", "Maximum2DDiameterSlice", "Maximum3DDiameter", "MeshVolume", "MinorAxisLength", "Sphericity", "SurfaceArea", "SurfaceVolumeRatio", "VoxelVolume"]
+    Grainfieldnames = [ "FloretName", "Elongation", "Flatness", "LeastAxisLength", "MajorAxisLength", "Maximum2DDiameterColumn", "Maximum2DDiameterRow", "Maximum2DDiameterSlice", "Maximum3DDiameter", "MeshVolume", "MinorAxisLength", "Sphericity", "SurfaceArea", "SurfaceVolumeRatio", "VoxelVolume", "EmptySurroundingSpace"]
+    with open('GrainData.csv', mode='w', newline='') as GrainData_csv:
+        writer = csv.DictWriter(GrainData_csv, fieldnames=Grainfieldnames)
+        writer.writeheader()
+    with open('GrowingSpaceData.csv', mode='w', newline='') as GrowingSpaceData_csv:
+        writer = csv.DictWriter(GrowingSpaceData_csv, fieldnames=fieldnames)
+        writer.writeheader()
+    with open('PaleaData.csv', mode='w', newline='') as PaleaData_csv:
         writer = csv.DictWriter(PaleaData_csv, fieldnames=fieldnames)
-        writer.writerow(PaleaFeatures)
-    
-
-    # Calculate the features, print result and append to the dictionary
-    print('Calculating shape features ' + str(FloretFilename) + ' for Lemma...',)
-    shapeFeatures = shape.RadiomicsShape(FloretLabel, LemmaMask)
-    result = shapeFeatures.execute()
-    print('done')
-
-    print('Calculated shape features for ' + str(FloretFilename) + ' Lemma: ')
-    for (key, val) in six.iteritems(result):
-        LemmaFeatures[key] = np.round(val, 4)
-        print('  ', key, ':', np.round(val, 4))
-    
-    with open('LemmaData.csv', mode='a', newline='') as LemmaData_csv:
+        writer.writeheader()
+    with open('LemmaData.csv', mode='w', newline='') as LemmaData_csv:
         writer = csv.DictWriter(LemmaData_csv, fieldnames=fieldnames)
-        writer.writerow(LemmaFeatures)
-
-
-    # Calculate the features, print result and append to the dictionary
-    print('Calculating shape features ' + str(FloretFilename) + ' for Shell...',)
-    shapeFeatures = shape.RadiomicsShape(FloretLabel, ShellMask)
-    result = shapeFeatures.execute()
-    print('done')
-
-    print('Calculated shape features for ' + str(FloretFilename) + ' Shell: ')
-    for (key, val) in six.iteritems(result):
-        ShellFeatures[key] = np.round(val, 4)
-        print('  ', key, ':', np.round(val, 4))
-    
-    with open('ShellData.csv', mode='a', newline='') as ShellData_csv:
+        writer.writeheader()
+    with open('ShellData.csv', mode='w', newline='') as ShellData_csv:
         writer = csv.DictWriter(ShellData_csv, fieldnames=fieldnames)
-        writer.writerow(ShellFeatures)
-    
-    print("Process complete for " + str(FloretFilename))
-    CompleteCounter = CompleteCounter + 1
-    print(str(CompleteCounter) + "/" + str(len(FloretFileNamesList)) + " Completed")
-    
+        writer.writeheader()
+   
+   
+    CompleteCounter = 0
+    #For each listed floret label, create a cavity label and extract statistics for each part
+    for FloretFilename in InputLabelList:
+        
+        #read segmentation/label file
+        reader.SetFileName(str(FloretFilename) + "seg.nii.gz")
+        FloretLabel = reader.Execute()
+        FloretLabel.SetSpacing((0.034, 0.034, 0.034)) #resolution of CT scanner in milimeters
+        
+        #obtain shell mask by thresholding labels 2 and 3 which are the palea and lemma respectively 
+        GrainMask = sitk.BinaryThreshold( FloretLabel, 1, 1, 1, 0 )
+        PaleaMask = sitk.BinaryThreshold( FloretLabel, 2, 2, 1, 0 )
+        LemmaMask = sitk.BinaryThreshold( FloretLabel, 3, 3, 1, 0 )
+        ShellMask = PaleaMask + LemmaMask #Shell mask obtained by elementwise addition of the masks
+        
+        #save result
+        os.chdir(DirDict['shellmask'])
+        sitkwriter.SetFileName(str(FloretFilename) + "ShellMask.nii.gz")
+        sitkwriter.Execute(ShellMask)
+        
+        "This section is for using pyradiomics to extract sphericity; length, width and height values; "
+        #Define Dictionaries in which the first the first entry is the name of the floret
+        GrainFeatures = {
+                "FloretName" : FloretFilename
+                }
+        PaleaFeatures = {
+                "FloretName" : FloretFilename
+                }
+        LemmaFeatures = {
+                "FloretName" : FloretFilename
+                }
+        ShellFeatures = {
+                "FloretName" : FloretFilename
+                }
+        
+        
+        #perform dilation on grain mask to obtain available growing space for the grain
+        #Dilation number (number of iterations of dilaion) is manually chosen with the growingspace argument
+        if growingspace>0:
+            Space,SpaceVolume = GrainGrowingSpace(GrainMask,ShellMask,growingspace)
+        
+        # Set the features to be calculated
+        # shapeFeatures.enableFeatureByName('Volume', True)
+        shapeFeatures.enableAllFeatures()
 
-print("Data extraction completed for all listed floret labels")
+        # Calculate the features, print result and append to the dictionary
+        print('Calculating shape features for ' + str(FloretFilename) + ' Grain...',)
+        shapeFeatures = shape.RadiomicsShape(FloretLabel, GrainMask)
+        
+        result = shapeFeatures.execute()
+        print('done')
+        
+        print('Calculated shape features for ' + str(FloretFilename) + ' Grain: ')
+        for (key, val) in six.iteritems(result):
+            GrainFeatures[key] = np.round(val, 4)
+            print('  ', key, ':', np.round(val, 4))
+        
+        #This is where we calculate raio of growingspace to surface area of grain and insert it into the csv
+        #GrainFeatures["PercentageOfCavityFilled"] = GrainFeatures["VoxelVolume"] / CavityFeatures["VoxelVolume"]
+        #GrainFeatures["BranPercentage"] =  BranVolume / GrainFeatures["VoxelVolume"]
+
+        with open('GrainData.csv', mode='a', newline='') as GrainData_csv:
+            writer = csv.DictWriter(GrainData_csv, fieldnames=fieldnames)
+            writer.writerow(GrainFeatures)
+        
+        
+        # Calculate the features, print result and append to the dictionary
+        print('Calculating shape features for ' + str(FloretFilename) + ' Palea...',)
+        shapeFeatures = shape.RadiomicsShape(FloretLabel, PaleaMask)
+        result = shapeFeatures.execute()
+        print('done')
+        
+        print('Calculated shape features for ' + str(FloretFilename) + ' Palea: ')
+        for (key, val) in six.iteritems(result):
+                PaleaFeatures[key] = np.round(val, 4)
+                print('  ', key, ':', np.round(val, 4))
+        
+        with open('PaleaData.csv', mode='a', newline='') as PaleaData_csv:
+            writer = csv.DictWriter(PaleaData_csv, fieldnames=fieldnames)
+            writer.writerow(PaleaFeatures)
+        
+
+        # Calculate the features, print result and append to the dictionary
+        print('Calculating shape features ' + str(FloretFilename) + ' for Lemma...',)
+        shapeFeatures = shape.RadiomicsShape(FloretLabel, LemmaMask)
+        result = shapeFeatures.execute()
+        print('done')
+
+        print('Calculated shape features for ' + str(FloretFilename) + ' Lemma: ')
+        for (key, val) in six.iteritems(result):
+            LemmaFeatures[key] = np.round(val, 4)
+            print('  ', key, ':', np.round(val, 4))
+        
+        with open('LemmaData.csv', mode='a', newline='') as LemmaData_csv:
+            writer = csv.DictWriter(LemmaData_csv, fieldnames=fieldnames)
+            writer.writerow(LemmaFeatures)
+
+
+        # Calculate the features, print result and append to the dictionary
+        print('Calculating shape features ' + str(FloretFilename) + ' for Shell...',)
+        shapeFeatures = shape.RadiomicsShape(FloretLabel, ShellMask)
+        result = shapeFeatures.execute()
+        print('done')
+
+        print('Calculated shape features for ' + str(FloretFilename) + ' Shell: ')
+        for (key, val) in six.iteritems(result):
+            ShellFeatures[key] = np.round(val, 4)
+            print('  ', key, ':', np.round(val, 4))
+        
+        with open('ShellData.csv', mode='a', newline='') as ShellData_csv:
+            writer = csv.DictWriter(ShellData_csv, fieldnames=fieldnames)
+            writer.writerow(ShellFeatures)
+        
+        print("Process complete for " + str(FloretFilename))
+        CompleteCounter = CompleteCounter + 1
+        print(str(CompleteCounter) + "/" + str(len(InputLabelList)) + " Completed")
+        
+
+    print("Data extraction completed for all listed floret labels")
     
     
 if __name__ == '__main__':
@@ -222,11 +247,12 @@ if __name__ == '__main__':
                         help='Output directory absolute path')
     parser.add_argument('-t', '--troubleshoot', action='store_true',
                         help='Save images  of intermediate steps threshold, components, and mask')
-    parser.add_argument('-g', '--troubleshoot', action='store_true',
-                        help='Save images  of intermediate steps threshold, components, and mask')
+    parser.add_argument('-g', '--growingspace', action='store_true',
+                        type=int, default=0,
+                        help='Number of dilaions of the space around the grain. <5 is recommended')
 
     args = parser.parse_args()
     print(args, flush=True)
-    main(args.inpath,args.outpath,troubleshoot=args.troubleshoot,normalize=args.normalize)
+    main(args.inpath,args.outpath,growingspace=args.growingspace,troubleshoot=args.troubleshoot)
     
     
